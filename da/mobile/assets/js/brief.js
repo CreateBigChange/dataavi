72f52728b438be7dbd7d20a19c9f9b2ae495d052
yangxiansheng <312479274@qq.com>
2016-08-05 01:07:14 +0800

    手机端完成

---
+++ da/mobile/assets/js/brief.js
@@ -0,0 +1,182 @@
+
+
+
+ /***************************正则匹配参数修改全局变量以及数据初始化**************/
+    str =window.location.search;
+    var reg = new RegExp(/\?user_id=(\d)+&sid=((.?)+)/);
+    arr=str.match(reg);
+    user_id=arr[1];
+    sid=arr[2];
+    num=0;
+    type="teleplay";
+    platform="all";
+    limit=20;
+    offset=0;
+    order_by="day_play_counts";
+    _="1470066582355";
+    $("[type='teleplay']").css("background","#d1cdc8");
+    //baseUrl =window.location.host;
+    baseUrl="";
+    // baseUrl="http://hnsdmp.com";
+     url=baseUrl+"/api/v1/user/"+user_id+"/playinfo";
+     detaiUrl =baseUrl+"/two.html?user_id="+user_id+"&sid="+sid;
+    /******************************搜索***********************************/
+    $("#searchVa").click(function () {
+        type ="variety";
+       window.location.href=baseUrl+"/two.html?name="+$("#searchInput").val()+"&type="+type+"&sid="+sid+"&user_id="+user_id;
+    })
+
+    $("#searchTv").click(function () {
+        type="teleplay";
+        window.location.href=baseUrl+"/two.html?name="+$("#searchInput").val()+"&type="+type+"&sid="+sid+"&user_id="+user_id;
+    })
+
+    /************************** type选择******************************/
+    $("#teleplay").click(function () {
+        $(".type").css("background","#ffffff");
+         type =$(this).attr("type");
+        $(this).css("background","#d1cdc8");
+        num=0;
+        offset=0;
+        $("#appendone").empty();
+        $("#appendtwo").empty();
+        pageajax(type,  platform  ,limit  ,offset  ,sid , order_by  ,_,url);
+    })
+
+    $("#variety").click(function () {
+        $(".type").css("background","#ffffff");
+        type =$(this).attr("type");
+        $(this).css("background","#d1cdc8");
+        num=0;
+        offset=0;
+        $("#appendone").empty();
+        $("#appendtwo").empty();
+        pageajax(type,  platform  ,limit  ,offset  ,sid , order_by  ,_,url);
+    })
+    /***************************platform选择*****************************************/
+    $(".platform").click(function () {
+        platform = $(this).attr("platform");
+        //清空所有子节点 相关参数设置
+        num=0;
+        offset=0;
+        $("#appendone").empty();
+        $("#appendtwo").empty();
+        pageajax(type,  platform  ,limit  ,offset  ,sid , order_by  ,_,url);
+    });
+
+    /************************************左右滑动参数配置 ********/
+
+
+
+    $(".inner div").css("width", screen.width);
+    $(".inner ").css("width", screen.width * 2);
+    $(".body ").css("height", screen.height-60);
+    $(".inner ").css("height", screen.height -300);
+    $("body").css("height", screen.height);
+
+    /*3.设定每次移动的距离,和，最多移动的次数*/
+    Flipsnap('.inner',{
+        distance:screen.width,
+        maxPoint:1       //最多移动的次数
+    });
+
+/****************** * 菜单模态框的配置*************************************/
+
+    function overlay() {
+        var e1 = document.getElementById('mymodal-overlay');
+        var e2 = document.getElementById('modal-data');
+        e1.style.visibility = (e1.style.visibility == "visible"  ) ? "hidden" : "visible";
+        var sm = $("#showModal");
+        if (sm.css("display") == "none")
+            sm.css("display", "block");
+        else
+            sm.css("display", "none");
+    }
+
+    $("#more").click(function () {
+        overlay();
+    })
+    $(".myButton2").click(function () {
+        overlay();
+    })
+    $(".myButton1").click(function () {
+        overlay();
+    })
+
+
+/**********************************************8向下滚动事件的配置及数据的获取 *************/
+    $(window).bind('scroll', function() {
+        show()
+    });
+    function show() {
+        if ($(window).scrollTop() + $(window).height() >= $(document).height()) {
+            num++;
+            offset=num*20;
+    
+            pageajax(type,  platform  ,limit  ,offset  ,sid , order_by  ,_,url);
+        }
+    }
+    /*********************数据重置*************************************************/
+
+
+/*******************************************************************
+ * function ajax 获取数据
+ * @param type
+ * @param platform
+ * @param limit
+ * @param offset
+ * @param sid
+ * @param order_by
+ * @param _
+ * @param url
+ */
+    function pageajax(type,  platform  ,limit  ,offset  ,sid , order_by  ,_,url)
+    {
+      
+        var htmlone ="";
+        var htmltwo ="";
+        ajaxUrl=url+"?type="+type+"&platform="+platform+"&limit="+limit+"&offset="+offset+"&sid="+sid+"&order_by="+order_by+"&_="+_;
+        
+        $.ajax({
+            url:ajaxUrl,
+            type:"get",
+            dataType:"json",
+            data:JSON.stringify({}),
+            success:function(data)
+            {
+               console.log(data);
+                if(data=='username or password error')
+                {
+                    alert("密码或者账号错误");
+                }
+                else
+                {
+                   for(var i =0;i<data.length;i++){
+                       htmlone+="<tr ><td><a class='mytr' href='"+detaiUrl+"&name="+data[i].name+"&type="+data[i].type+"'>"+data[i].name+"</a></td><td>"+data[i].avg_play+"</td><td>"+data[i].all_play_counts+"</td></tr>";
+                       htmltwo+="<tr ><td>"+data[i].day_play_counts+"</td><td>"+data[i].current_number+"/"+data[i].all_number+"</td><td>"+data[i].time_at+"</td></tr>";
+                   }
+                    $("#appendone").append(htmlone);
+                    $("#appendtwo").append(htmltwo);
+                }
+            },
+            error:function()
+            {
+                alert("请求失败");
+            }
+        });
+    }
+
+
+/***********************************angular 数据绑定 初始化 * ************************/
+    var app = angular.module('myApp', []);
+    app.controller('customersCtrl', function($scope, $http) {
+        $http.get(baseUrl+"/api/v1/user/"+user_id+"/playinfo?type=teleplay&platform=all&limit=20&offset=0&sid="+sid+"&order_by=day_play_counts&_=1470066582355")
+                .success(function(response) {
+                    $scope.names = response;
+                    for(var i= 0;i<response.length;i++){
+                        $scope.names[i].url=detaiUrl+"&name="+response[i].name+"&type="+response[i].type;
+                    }
+                    
+                });
+    });
+
---
+++ da/mobile/assets/js/scripts.js
@@ -0,0 +1,77 @@
+
+jQuery(document).ready(function() {
+
+    /**
+     * 前段验证密码和账号
+     */
+    $('.page-container form').submit(function(){
+        var username = $(this).find('.username').val();
+        var password = $(this).find('.password').val();
+        if(username == '') {
+            $(this).find('.error').fadeOut('fast', function(){
+                $(this).css('top', '27px');
+            });
+            $(this).find('.error').fadeIn('fast', function(){
+                $(this).parent().find('.username').focus();
+            });
+            return false;
+        }
+        if(password == '') {
+            $(this).find('.error').fadeOut('fast', function(){
+                $(this).css('top', '96px');
+            });
+            $(this).find('.error').fadeIn('fast', function(){
+                $(this).parent().find('.password').focus();
+            });
+            return false;
+        }
+    });
+
+    $('.page-container form .username, .page-container form .password').keyup(function(){
+        $(this).parent().find('.error').fadeOut('fast');
+    });
+
+    /**
+     * 验证用户的ajax
+     * @param username
+     * @param passwd
+     * @param url
+     */
+
+function ajax(username,passwd,url,baseUrl)
+    {
+        $.ajax({
+        url:url,
+        type:"post",
+        dataType:"json",
+        data:JSON.stringify({username:username, passwd:passwd}),
+        success:function(user)
+        {
+                if(user=='username or password error')
+                {
+                     alert("密码或者账号错误");
+                }   
+                else
+                {
+                    window.location.href=baseUrl+"/mobile/Brief.html?user_id="+user.id+"&sid="+user.sid;
+                }
+        },
+        error:function()
+        {
+            alert("请求失败");
+        }
+        });
+    }
+    
+$("#login").click(function () {
+    var userName = $("#userName").val();
+    var passWord = $("#passWord").val();
+   
+     baseUrl="";
+     //baseUrl =window.location.host
+     // baseUrl = "http://hnsdmp.com";
+    var url=baseUrl+"/api/v1/user/login";
+    
+    ajax(userName,passWord,url,baseUrl);
+})
+});
